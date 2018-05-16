795e5929a28bde6e07bd9b9027276a06c3c57b4d
Evan White <evwhite2993@gmail.com>
2018-05-15 23:43:10 -0700

    endpoints/fizzbuzz-1: Add unit tests for fizzbuzz endpoint.

---
+++ src/test/java/org/evwhite/algorithmapi/resource/GenericControllerTest.java
@@ -0,0 +1,34 @@
+package org.evwhite.algorithmapi.resource;
+
+import org.evwhite.algorithmapi.Answer;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GenericControllerTest {
+
+    private GenericController subject = new GenericController();
+
+    @Test
+    public void getFizzBuzzReturnsAnswerObject() {
+        int defaultLimit = 50;
+
+        Object response = subject.getFizzBuzz(defaultLimit);
+
+        assertThat(response, instanceOf(Answer.class));
+    }
+
+    @Test
+    public void getFizzBuzzAnswerContainsAccurateResultString() {
+        int specifiedLimit = 20;
+        String expectedStringResult = "1 | 2 | fizz | 4 | buzz | fizz | 7 | 8 | fizz | buzz | 11 |" +
+                " fizz | 13 | 14 | fizzbuzz | 16 | 17 | fizz | 19";
+
+        Answer response = subject.getFizzBuzz(specifiedLimit);
+
+        String actualStringResult = response.getStringResult();
+        assertThat(actualStringResult, is(expectedStringResult));
+    }
+}
