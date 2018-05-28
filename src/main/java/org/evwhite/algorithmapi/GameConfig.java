be89f65ba46b0a2587583b6ae56e156d9cc56e73
Evan White <evwhite2993@gmail.com>
2018-05-28 15:43:11 -0700

    endpoints/gameoflife-2: Add grid dimensions to GameConfig request representation.

--- src/main/java/org/evwhite/algorithmapi/GameConfig.java
+++ src/main/java/org/evwhite/algorithmapi/GameConfig.java
@@ -3,8 +3,26 @@ package org.evwhite.algorithmapi;
 import java.util.List;
 
 public class GameConfig {
+    private int gridHeight;
+    private int gridWidth;
     private List<Coordinate> startingAlive;
     private int generations;
+
+    public int getGridHeight() {
+        return gridHeight;
+    }
+
+    public void setGridHeight(int gridHeight) {
+        this.gridHeight = gridHeight;
+    }
+
+    public int getGridWidth() {
+        return gridWidth;
+    }
+
+    public void setGridWidth(int gridWidth) {
+        this.gridWidth = gridWidth;
+    }
 
     public List<Coordinate> getStartingAlive() {
         return startingAlive;
